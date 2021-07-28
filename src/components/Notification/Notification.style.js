9a66426d709f920bb3474d36b85eb9b79a0c607c
ekarazas <karazas7@gmail.com>
2021-07-28 09:41:16 +0300

    some style fix

--- src/components/Notification/Notification.style.js
+++ src/components/Notification/Notification.style.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const StyledNotification = styled.div`
+  width: 100%;
   padding: 1rem;
   border-radius: 0.3rem;
   background: ${(props) => (props.type === "success" ? "#239985" : "#df5555")};
   color: rgb(250, 250, 250);
-  margin: 0 0 1rem 0;
+  margin: 0;
   position: relative;
   line-break: auto;
+  box-sizing: border-box;
 `;
 
 export const CloseButton = styled.button`
